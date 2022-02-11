9ff9624b5e1b75e5dd188301c78bb7d41ec607c0
crab <92041779+crabbo-rave@users.noreply.github.com>
2022-02-11 11:07:42 -0500

    examples

--- Examples.lean
+++ Examples.lean
@@ -1,8 +1,13 @@
 import Tuple
 open Tuple
 
-def tuplePatternMatch (tup: Tuple) :=
+def tuplePatternMatch (tup: Tuple [Nat, Nat]) :=
   match tup with
-  | (1; 2) => true
-  | (2; 1) => true
+  | ⟪1, 2⟫ => true
+  | ⟪2, 1⟫ => true
   | _ => false
+
+#eval tuplePatternMatch ⟪1, 2⟫
+#eval tuplePatternMatch ⟪1, 3⟫
+
+#eval head ⟪1, 2, 3⟫
--- TupleMethods.lean
+++ TupleMethods.lean
@@ -7,7 +7,7 @@ def head {α : Type} {αs : List Type} (tup: Tuple (α :: αs)) : α :=
   match tup with
   | Tuple.cons x xs => x
 
-def tail {α : Type} {αs : List Type} (tup: Tuple (α :: αs)) : αs :=
+def tail {α : Type} {αs : List Type} (tup: Tuple (α :: αs)) : Tuple αs :=
   match tup with
   | Tuple.cons x xs => xs
 
