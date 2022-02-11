01f0671b23e9faf53b850212563b8d759822be7b
crab <92041779+crabbo-rave@users.noreply.github.com>
2022-02-11 10:43:30 -0500

    Tuple type

--- Tuple.lean
+++ Tuple.lean
@@ -5,30 +5,31 @@ namespace Tuple
 
 open Nth
 
-instance (α β : Type _) : Nth (α × β) 0 α where
-  nth p := p.1
+-- instance (α β : Type _) : Nth (α × β) 0 α where
+--   nth p := p.1
 
-instance (α β γ : Type _) [Nth β n γ] : Nth (α × β) (n + 1) γ where
-  nth p := nth n p.2
+-- instance (α β γ : Type _) [Nth β n γ] : Nth (α × β) (n + 1) γ where
+--   nth p := nth n p.2
 
-@[defaultInstance 0]
-instance : Nth α 0 α where
-  nth x := x
+-- @[defaultInstance 0]
+-- instance : Nth α 0 α where
+--   nth x := x
 
-syntax (name := Tuple) "(" term ";" sepBy1(term, ";", "; ") ")" : term
+inductive Tuple : List Type → Type 1 where
+  | unit : Tuple []
+  | cons {α : Type} (x : α) {αs : List Type} (xs : Tuple αs) : Tuple (α :: αs)
+
+syntax "⟪" sepBy(term, ",", ", ") "⟫" : term
 macro_rules
-  | `(($a; $b)) => `(Prod.mk $a $b)
-  | `(($a; $b; $xs;*)) => `(Prod.mk $a ($b; $xs;*))
+  | `(⟪⟫) => `(Tuple.unit)
+  | `(⟪$x⟫) => `(Tuple.cons $x Tuple.unit)
+  | `(⟪$x, $xs,*⟫) => `(Tuple.cons $x ⟪$xs,*⟫)
 
-notation:max p "^" i => nth i p
-notation:max p "#" i => nth i p
-notation:max p "@" i => nth i p
-notation:max p "[" i "]" => nth i p
-notation:max p "::" i => nth i p
-notation:max p "∧" i => nth i p
-
-inductive Tuple where
-  | empty : Tuple
-  | tuple : Tuple → Tuple
+-- notation:max p "^" i => nth i p
+-- notation:max p "#" i => nth i p
+-- notation:max p "@" i => nth i p
+-- notation:max p "[" i "]" => nth i p
+-- notation:max p "::" i => nth i p
+-- notation:max p "∧" i => nth i p
 
 end Tuple
--- TupleMethods.lean
+++ TupleMethods.lean
@@ -1,3 +1,14 @@
+import Tuple
+open Tuple
+
 namespace Tuple
+
+def head {α : Type} {αs : List Type} (tup: Tuple (α :: αs)) : α :=
+  match tup with
+  | Tuple.cons x xs => x
+
+def tail {α : Type} {αs : List Type} (tup: Tuple (α :: αs)) : αs :=
+  match tup with
+  | Tuple.cons x xs => xs
 
 end Tuple
