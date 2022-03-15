6f8050746f770623edf9e85c8fa4bc70f1be81d8
static-binary <vatisprocurator@gmail.com>
2022-03-14 22:50:42 -0400

    Update Syntax.lean

--- Soup/Range/Syntax.lean
+++ Soup/Range/Syntax.lean
@@ -1,4 +1,13 @@
+import Std
 import Soup.Range.Functions
 
-set_option quotPrecheck false in
-notation:max "[" st "..." sp "]" => [st:sp].toList ++ [sp]
+-- set_option quotPrecheck false in
+-- notation:max "[" st "..." sp "]" => [st:sp].toList ++ [sp]
+
+syntax "[" term "..." term "]" : term
+macro_rules
+  | `([$st ... $sp]) => `(
+    { start := $st, stop := $sp : Std.Range }.toList ++ [$sp]
+  )
+
+#eval [1 ... 5]
