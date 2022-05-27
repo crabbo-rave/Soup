677067140cdf4b31e84fdcac7e6ea8be8fd0b4a0
crab <92041779+crabbo-rave@users.noreply.github.com>
2022-05-27 09:38:07 -0400

    Better version of String.reverse

--- Soup/String/Functions.lean
+++ Soup/String/Functions.lean
@@ -1,5 +1,6 @@
 namespace String
 
-def String.reverse := (String.intercalate "") ∘ (List.map toString) ∘ List.reverse ∘ String.toList
+def String.reverse (s : String) : String :=
+  ⟨s.data.reverse⟩
 
 end String
